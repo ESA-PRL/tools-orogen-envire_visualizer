834a0e2bef3b621288ae0822cf97351c9b9f42b4
Ricardo Sanchez <ricardosan@uma.es>
2018-06-11 10:04:43 +0200

    initial commit by rock-create-orogen

---
+++ envire_visualizerTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef envire_visualizer_TYPES_HPP
+#define envire_visualizer_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace envire_visualizer {
+}
+
+#endif
+
